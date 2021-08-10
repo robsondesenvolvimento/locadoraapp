a3167498500e40600e2d2b895323f5295994c154
Robson Candido dos Santos Alves <robson@zylix.com.br>
2021-08-10 09:06:13 -0300

    fix update readme

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -1,14 +1,16 @@
-const yaml = require('js-yaml');
-const fs = require('fs');
+const yaml = require('js-yaml')
+const fs = require('fs')
 
-const env = process.env.NODE_ENV || 'prod';
+const env = process.env.NODE_ENV || 'prod'
 
-if(env == "prod")
-    console.log("Modo de produção iniciado.")
-else if (env == "dev")
-    console.log("Mode de desenvolvimento iniciado.")
+if (env === 'prod') {
+    console.log('Modo de produção iniciado.')
+}    
+else if (env === 'dev') {
+    console.log('Mode de desenvolvimento iniciado.')
+}
 
-const file = fs.readFileSync(`./src/config/${env}.yaml`, 'utf8');
-const configuration = yaml.load(file);
+const file = fs.readFileSync(`./src/config/${env}.yaml`, 'utf8')
+const configuration = yaml.load(file)
 
-module.exports = configuration;
+module.exports = configuration
